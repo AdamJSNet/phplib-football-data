a2f6b2ece8a2c37061e0a5dc8808644ecab427f0
Adam Scholey <hello@adamjs.net>
2018-12-30 11:11:27 +0000

    Reformat FootballData class

--- FootballData.php
+++ FootballData.php
@@ -30,11 +30,13 @@ class FootballData {
      * 
      * @param Integer $id
      * @return array
-     */        
+     */
     public function findCompetitionById($id) {
         $resource = 'competitions/' . $id;
-        $response = file_get_contents($this->baseUri . $resource, false, 
-                                      stream_context_create($this->reqPrefs));
+        $response = file_get_contents(
+            $this->baseUri . $resource, false,
+            stream_context_create($this->reqPrefs)
+        );
         
         return json_decode($response);
     }
@@ -46,40 +48,48 @@ class FootballData {
      * @param DateString 'Y-m-d' $end
      * 
      * @return array of matches
-     */    
+     */
     public function findMatchesForDateRange($start, $end) {
         $resource = 'matches/?dateFrom=' . $start . '&dateTo=' . $end;
 
-        $response = file_get_contents($this->baseUri . $resource, false, 
-                                      stream_context_create($this->reqPrefs));
-        
+        $response = file_get_contents(
+            $this->baseUri . $resource, false,
+            stream_context_create($this->reqPrefs)
+        );
+
         return json_decode($response);
     }
     
     public function findMatchesByCompetitionAndMatchday($c, $m) {
         $resource = 'competitions/' . $c . '/matches/?matchday=' . $m;
 
-        $response = file_get_contents($this->baseUri . $resource, false, 
-                                      stream_context_create($this->reqPrefs));
+        $response = file_get_contents(
+            $this->baseUri . $resource, false,
+            stream_context_create($this->reqPrefs)
+        );
         
         return json_decode($response);
     }
 
     public function findStandingsByCompetition($id) {
-	$resource = 'competitions/' . $id . '/standings';
-        $response = file_get_contents($this->baseUri . $resource, false, 
-                                      stream_context_create($this->reqPrefs));
+	    $resource = 'competitions/' . $id . '/standings';
+        $response = file_get_contents(
+            $this->baseUri . $resource, false,
+            stream_context_create($this->reqPrefs)
+        );
 
         return json_decode($response);
     }
 
     
     public function findHomeMatchesByTeam($teamId) {
+        // http://api.football-data.org/v2/teams/62/matches?venue=home
         $resource = 'teams/' . $teamId . '/matches/?venue=HOME';
-        //http://api.football-data.org/v2/teams/62/matches?venue=home
 
-        $response = file_get_contents($this->baseUri . $resource, false, 
-                                      stream_context_create($this->reqPrefs));
+        $response = file_get_contents(
+            $this->baseUri . $resource, false,
+            stream_context_create($this->reqPrefs)
+        );
         
         return json_decode($response)->matches;
     }
@@ -92,8 +102,10 @@ class FootballData {
      */
     public function findMatchById($id) {
         $resource = 'matches/' . $id;
-        $response = file_get_contents($this->baseUri . $resource, false, 
-                                      stream_context_create($this->reqPrefs));
+        $response = file_get_contents(
+            $this->baseUri . $resource, false,
+            stream_context_create($this->reqPrefs)
+        );
         
         return json_decode($response);
     }
@@ -106,8 +118,10 @@ class FootballData {
      */    
     public function findTeamById($id) {
         $resource = 'teams/' . $id;
-        $response = file_get_contents($this->baseUri . $resource, false, 
-                                      stream_context_create($this->reqPrefs));
+        $response = file_get_contents(
+            $this->baseUri . $resource, false,
+            stream_context_create($this->reqPrefs)
+        );
         
         return json_decode($response);
     }
@@ -117,11 +131,13 @@ class FootballData {
      * 
      * @param string $keyword
      * @return list of team objects
-     */    
+     */
     public function searchTeam($keyword) {
         $resource = 'teams/?name=' . $keyword;
-        $response = file_get_contents($this->baseUri . $resource, false, 
-                                      stream_context_create($this->reqPrefs));
+        $response = file_get_contents(
+            $this->baseUri . $resource, false,
+            stream_context_create($this->reqPrefs)
+        );
         
         return json_decode($response);
     }    
